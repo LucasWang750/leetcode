6276f5ece27c66ca43f8ece0ff9c42533a90d672
Lucas Wang <47483544+LucasWang750@users.noreply.github.com>
2024-02-01 20:54:25 -0800

    Time: 83 ms (88.77%), Space: 19.3 MB (99.64%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashmap = {}
+        for i in strs:
+            sorted_str = "".join(sorted(i))
+            if sorted_str in hashmap:
+                hashmap[sorted_str].append(i)
+            else:
+                hashmap[sorted_str] = [i]
+        
+        
+        return list(hashmap.values())
