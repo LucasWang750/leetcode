f4e2d8286a3000e1a30ee2e2772159f6009ccc43
Lucas Wang <47483544+LucasWang750@users.noreply.github.com>
2024-03-08 13:29:52 -0800

    Time: 38 ms (48.10%), Space: 16.5 MB (87.01%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,12 +1,7 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         index = -1
-        # digits[index] += 1
         carryOver = 1
-        # carryOver = 1 if digits[index] == 10 else 0
-        # if carryOver == 1:
-        #     digits[index] = 0
-        # index = -2
         while -index <= len(digits):
             digits[index] += carryOver
             carryOver = 1 if digits[index] == 10 else 0
