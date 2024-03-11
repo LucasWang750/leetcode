624e6830db716f31755863ac0979702e417991c0
Lucas Wang <47483544+LucasWang750@users.noreply.github.com>
2024-03-11 00:26:20 -0700

    Time: 40 ms (40.07%), Space: 16.7 MB (91.17%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.py
+++ 0541-reverse-string-ii/0541-reverse-string-ii.py
@@ -1,17 +1,8 @@
 class Solution:
     def reverseStr(self, s: str, k: int) -> str:
-        length = len(s)
         s = list(s)
-        left = 0
-        left_index = left
-        while True:
-            right = (left + k - 1) if (left + k - 1) <= (length - 1) else (length - 1)
-            right_index = right
-            while left_index < right_index:
-                s[left_index], s[right_index] = s[right_index], s[left_index]
-                left_index += 1
-                right_index -= 1
-            left += 2 * k
-            left_index = left
-            if left > length:
-                return ''.join(s)
+        
+        for i in range(0, len(s), 2*k):
+            s[i:i+k] = reversed(s[i:i+k])
+        return "".join(s)
+        
