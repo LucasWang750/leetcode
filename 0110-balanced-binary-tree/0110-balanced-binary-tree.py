06f0bfdda21e646de00aa5eadc995a50a5b3b46d
Lucas Wang <47483544+LucasWang750@users.noreply.github.com>
2024-03-19 16:12:07 -0700

    Time: 57 ms (8.73%), Space: 17.6 MB (86.96%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -6,16 +6,41 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        def height(root):
-            if not root:
-                return -1
-            return 1 + max(height(root.left), height(root.right))
+#         def height(root):
+#             if not root:
+#                 return -1
+#             return 1 + max(height(root.left), height(root.right))
         
+#         def dfs(root):
+#             if root:
+#                 height_left = height(root.left)
+#                 height_right = height(root.right)
+#                 return abs(height_left - height_right) < 2 and dfs(root.left) and dfs(root.right)
+#             else:
+#                 return True
+#         return dfs(root)
+        def height(root):
+            if root:
+                root.val = max(height(root.left), height(root.right)) + 1
+                return root.val
+            else:
+                return -1
+        height(root)
         def dfs(root):
             if root:
-                height_left = height(root.left)
-                height_right = height(root.right)
-                return abs(height_left - height_right) < 2 and dfs(root.left) and dfs(root.right)
+                if (root.left and root.right):
+                    return abs(root.left.val - root.right.val) < 2 and dfs(root.left) and dfs(root.right)
+                elif (root.left):
+                    return root.val < 2 and dfs(root.left)
+                elif (root.right):
+                    return root.val < 2 and dfs(root.right)
+                return root.val < 2
             else:
                 return True
+        # def p(root):
+        #     if root:
+        #         p(root.left)
+        #         print(root.val)
+        #         p(root.right)
+        # p(root)
         return dfs(root)
