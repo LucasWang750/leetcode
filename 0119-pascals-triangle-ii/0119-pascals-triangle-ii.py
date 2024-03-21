4aa89eb891c752963bd8ffa8f6011d1de9a440b7
Lucas Wang <47483544+LucasWang750@users.noreply.github.com>
2024-03-20 18:30:52 -0700

    Time: 39 ms (28.06%), Space: 16.6 MB (37.13%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -3,6 +3,6 @@ class Solution:
         arr = [1] * (rowIndex + 1)
         for i in range(rowIndex + 1):
             for j in range(i-1, 0, -1):
-                arr[j] = arr[j-1] + arr[j]
+                arr[j] += arr[j-1]
         return arr
         
