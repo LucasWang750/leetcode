04ead334cb209eccaf166e71ffd900e9d1062bc6
Lucas Wang <47483544+LucasWang750@users.noreply.github.com>
2024-03-21 11:05:55 -0700

    Time: 690 ms (91.17%), Space: 27.4 MB (35.19%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -4,11 +4,11 @@ class Solution:
         min_price = float('inf')
         
         for i in range(len(prices)):
-            min_price = min(prices[i], min_price)
-            # if prices[i] < min_price:
-            #     min_price = prices[i]
-            max_profit = max(max_profit, prices[i] - min_price)
-            # elif prices[i] - min_price > max_profit:
-            #     max_profit = prices[i] - min_price
+            # min_price = min(prices[i], min_price)
+            if prices[i] < min_price:
+                min_price = prices[i]
+            # max_profit = max(max_profit, prices[i] - min_price)
+            elif prices[i] - min_price > max_profit:
+                max_profit = prices[i] - min_price
         
         return max_profit
