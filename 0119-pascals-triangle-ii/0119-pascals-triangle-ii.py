1e244c3c9334508db98767af664872e3de061d0e
Lucas Wang <47483544+LucasWang750@users.noreply.github.com>
2024-03-20 18:27:33 -0700

    Time: 38 ms (37.06%), Space: 16.5 MB (37.13%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -4,15 +4,15 @@ class Solution:
         
         for i in range(rowIndex + 1):
             temp = arr[:]
-            # left = 1
-            # right = i - 1
-            # while left <= right:
-            #     temp[left] = arr[left-1] + arr[left]
-            #     temp[right] = temp[left]
-            #     left += 1
-            #     right -= 1
-            for j in range(1, i):
-                temp[j] = arr[j-1] + arr[j]
+            left = 1
+            right = i - 1
+            while left <= right:
+                temp[left] = arr[left-1] + arr[left]
+                temp[right] = temp[left]
+                left += 1
+                right -= 1
+            # for j in range(1, i):
+            #     temp[j] = arr[j-1] + arr[j]
             arr = temp[:]
         return arr
         
