fbdeb9a0cf571d6ac5051c2cdcde0892619407b2
Lucas Wang <47483544+LucasWang750@users.noreply.github.com>
2024-03-31 19:51:04 -0700

    Time: 51 ms (74.50%), Space: 16.8 MB (44.44%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -2,13 +2,9 @@ class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         hashmap = collections.Counter(magazine)
         hashmap2 = collections.Counter(ransomNote)
-        
-        print(hashmap)
-        print(hashmap2)
         for i in hashmap2:
             if i in hashmap and hashmap[i] >= hashmap2[i]:
                 continue
             else:
                 return False
-            # print(i)
         return True
